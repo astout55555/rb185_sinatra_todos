f45f6701e07bb641797f4fc8efb3aedf7d8e41c7
astout55555 <alex.the.stout5@gmail.com>
2023-09-12 13:18:53 -0700

    Catching up / updating before working through next assignments.

--- Gemfile
+++ Gemfile
@@ -4,7 +4,10 @@ ruby "3.2.0"
 gem "sinatra", "~>1.4.7"
 gem "sinatra-contrib"
 gem "erubis"
+gem "pg"
+
+gem "webrick"
 
 group :production do
-  gem "puma"
+  gem "puma", "~> 4.3.12"
 end
---
+++ schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE lists(
+  id serial PRIMARY KEY,
+  name varchar(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE todo(
+  id serial PRIMARY KEY,
+  name varchar(255) NOT NULL,
+  completed boolean NOT NULL DEFAULT false,
+  list_id int NOT NULL REFERENCES lists(id) ON DELETE CASCADE
+);
